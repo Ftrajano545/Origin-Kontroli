b7f200e301f06b7499755131ea96d9364a76b7f4
Fabio <suziotrajano@gmail.com>
2025-08-29 21:47:12 -0300

    crud

--- pages/admin/api.js
+++ pages/admin/api.js
@@ -1,20 +1,40 @@
+let url = "https://689e79853fed484cf87738b7.mockapi.io/Produtos"
 async function produtos() {
-    let url = "https://689e79853fed484cf87738b7.mockapi.io/Produtos"
     let response = await fetch(url)
     let data = await response.json()
 
-    for(let i = 0; i < data.length; i++){
+    for (let i = 0; i < data.length; i++) {
         let cadaProd = data[i]
-        let container = document.querySelector("#leBodyproductsTab")
-        let html = `
-        <tr>
-                <td>${cadaProd.nome}</td>git commit -m "Primeiro commit"
+        let container = document.querySelector("#productsTableBody")
 
-                <td>${cadaProd.codigo}</td>
-                <td>${cadaProd.quantidade}</td>
-                <td>${cadaProd.estoqueMin}</td>
-                <td>${cadaProd.preco}</td>
-                <td>${cadaProd.descricaoProd}</td>
+        let alerta = `
+        <img width="20" height="20" src="https://img.icons8.com/ios-filled/20/FA5252/error--v1.png" alt="error--v1"/>
+
+        `
+
+        let status = ''
+        let quantidade = cadaProd.quantity
+        let minStock = cadaProd.minStock
+        
+        quantidade < minStock ? status = alerta : status = "Em estoque"
+
+
+        let html = `
+            <tr>
+                <td>
+                    <p>${cadaProd.name}</p>
+                    <p class="descricao">${cadaProd.description}</p>
+                </td>
+                <td>${cadaProd.code}</td>
+                <td>${cadaProd.category}</td>
+                <td>${cadaProd.quantity}</td>
+                <td>${cadaProd.minStock}</td>
+                <td>${cadaProd.price}</td>
+                <td>${status}</td>
+                <td>
+                    <button onclick="openEditModal('${cadaProd.id}')">Editar</button>
+                    <button onclick="deletaProduto('${cadaProd.id}')">Excluir</button>
+                </td>
                 
             </tr>`
         container.innerHTML += html
@@ -23,22 +43,21 @@ async function produtos() {
 produtos()
 async function adicionaProduto() {
 
-    let url = "https://689e79853fed484cf87738b7.mockapi.io/Produtos"
-    let nomeProd = document.querySelector("#productName").value
-    let codigo = document.querySelector("#productCode").value
-    let quantidade = document.querySelector("#productQuantity").value
-    let minimo = document.querySelector("#productMinStock").value
-    let preco = document.querySelector("#productPrice").value
-    let descricao = document.querySelector("#productDescription").value
-    
+    let name = document.querySelector("#productName").value
+    let code = document.querySelector("#productCode").value
+    let quantity = document.querySelector("#productQuantity").value
+    let minStock = document.querySelector("#productMinStock").value
+    let price = document.querySelector("#productPrice").value
+    let description = document.querySelector("#productDescription").value
 
-    let novoProd= {
-        'nome': nome,
-        'codigo': codigo,
-        'quantidade': quantidade,
-        'estoqueMin': estoqueMin,
-        'preco': preco,
-        'descricao': descricao
+
+    let novoProd = {
+        'name': name,
+        'code': code,
+        'quantity': quantity,
+        'minStock': minStock,
+        'price': price,
+        'description': description
     }
     let response = await fetch(url, {
         method: 'POST',
@@ -46,5 +65,69 @@ async function adicionaProduto() {
         body: JSON.stringify(novoProd)
     })
     window.location.reload()
+
 }
-adicionaProduto()
+//deletar
+async function deletaProduto(id) {
+
+    let response = await fetch(`${url}/${id}`, {
+        method: "DELETE"
+    })
+
+    window.location.reload()
+}
+    
+
+
+
+function openModal() {
+    document.querySelector("#productModal").style.display = "flex"
+}
+
+function fecharModal() {
+    document.querySelector("#productModal").style.display = "none"
+
+}
+function openEditModal(id) {
+    document.querySelector("#productEditModal").style.display = "flex"
+    getId(id)
+}
+
+function fecharEditModal() {
+    document.querySelector("#productEditModal").style.display = "none"
+
+}
+
+//put
+
+async function getId(id) {
+    document.querySelector("#getId").value = id
+}
+
+async function updateProduct() {
+    let id = document.querySelector("#getId").value
+
+    let name = document.querySelector("#productNameEdit").value
+    let code = document.querySelector("#productCodeEdit").value
+    let quantity = document.querySelector("#productQuantityEdit").value
+    let minStock = document.querySelector("#productMinStockEdit").value
+    let price = document.querySelector("#productPriceEdit").value
+    let description = document.querySelector("#productDescriptionEdit").value
+
+    let novoProd = {
+        'name': name,
+        'code': code,
+        'quantity': quantity,
+        'minStock': minStock,
+        'price': price,
+        'description': description
+    }
+
+    let response = await fetch(`${url}/${id}`,{
+        method: 'PUT',
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify(novoProd)
+    })
+        window.location.reload()
+
+}
